33960f239e1184f8d97380b8df6ffe5f7499d05c
kjameslubin <kieren1@gmail.com>
2016-06-02 03:11:49 -0400

    big changes - new type class, working monitor polling. Need to make more type class instances, wire STM into error handling in differente executables and connect kafka to strato-api and strato-monitor

--- lib/API/Model/Status.hs
+++ lib/API/Model/Status.hs
@@ -8,8 +8,8 @@ module API.Model.Status where
 import Data.Aeson.TH
 
 data QuarryStatus = QuarryStatus {
-    message :: String,
-    timestamp :: String   -- replace with UTCTime
+    quarryMessage :: String,
+    quarryTimestamp :: String   -- replace with UTCTime
 } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''QuarryStatus)
